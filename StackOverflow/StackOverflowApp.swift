d56280ba0b1ea38b2dcfa087245283e9c277b2f3
Evgeny <Pushkarev>
2026-05-28 21:11:07 +0400

    Initial Commit

---
+++ StackOverflow/StackOverflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  StackOverflowApp.swift
+//  StackOverflow
+//
+//  Created by Evgeny on 28.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct StackOverflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
